f57314e2c066de8a2280d76d5bb2138698d43874
arpitpandey291105-coder <arpitpandey291105@gmail.com>
2026-05-24 00:06:41 +0530

    Hide Firebase keys using env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbG1CULLDuxzTwtGaj7TGpAVbop7E2cAI",
-  authDomain: "finance-pro-a3a9f.firebaseapp.com",
-  projectId: "finance-pro-a3a9f",
-  storageBucket: "finance-pro-a3a9f.firebasestorage.app",
-  messagingSenderId: "445397802662",
-  appId: "1:445397802662:web:d2f6f0f1e2713c35c9de67"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
